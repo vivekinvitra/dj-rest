3ac14e7eb681319978a29bef8a416e830a9cc553
Berke KIRAN <berkekiranofficial@hotmail.com>
2021-05-04 19:05:24 +0300

    Django Product Admin Image Gallery

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category, Product
+from product.models import Category, Product, Images
+
+
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,7 +17,14 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'price', 'status']
     list_filter = ['status', 'category']
+    inlines = [ProductImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'image']
+    list_filter = ['status', 'product']
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImagesAdmin)
--- product/models.py
+++ product/models.py
@@ -41,3 +41,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title

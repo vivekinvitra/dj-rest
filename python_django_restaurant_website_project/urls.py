4d5afd9beea548b13c95aff6486563d283e296b6
Berke KIRAN <berkekiranofficial@hotmail.com>
2021-05-04 16:51:29 +0300

    Django Product App and Category Database Table Creation

--- python_django_restaurant_website_project/urls.py
+++ python_django_restaurant_website_project/urls.py
@@ -1,4 +1,4 @@
-"""django_restaurant_website_project URL Configuration
+"""python_django_restaurant_website_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('home/', include('home.urls')),
+    path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
 ]

74b962874a0c1d28490bc719de74262b55c1be8c
Berke KIRAN <berkekiranofficial@hotmail.com>
2021-05-04 17:27:16 +0300

    Django Category and Product Model Relationships

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category
+from product.models import Category, Product
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -9,4 +9,10 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ['status']
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'status']
+    list_filter = ['status', 'category']
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -17,3 +17,27 @@ class Category(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    detail = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
